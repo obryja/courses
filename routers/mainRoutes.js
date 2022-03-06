17db3c11090fcd2cff0c32f9f91d3fae66120cf6
obryja <bryja.ola@gmail.com>
2022-03-06 17:57:23 +0100

    course site

--- routers/mainRoutes.js
+++ routers/mainRoutes.js
@@ -4,8 +4,8 @@ router.get('/', (req, res) => {
     res.render('index', {title: 'Kursy'})
 })
 
-router.get('/course', (req, res) => {
-    res.render('course', {title: 'Kurs'})
+router.get('/course/:courseIndex', (req, res) => {
+    res.render('course', {title: 'Kurs', courseIndex: req.params.courseIndex})
 })
 
 router.get('/coursesByCategory', (req, res) => {
